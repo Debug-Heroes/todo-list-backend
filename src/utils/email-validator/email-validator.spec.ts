047a57a4164aa535182532f91c68ad13276cc284
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-14 11:51:52 -0300

    test: ensure EmailValidator returns true if isEmail succeed

--- src/utils/email-validator/email-validator.spec.ts
+++ src/utils/email-validator/email-validator.spec.ts
@@ -1,6 +1,12 @@
 import validator from "validator"
 import { EmailValidator } from './email-validator'
 
+jest.mock('validator', () => ({
+  isEmail: () => {
+    return true
+  }
+}))
+
 describe('EmailValidator', () => {
   it('Should call isEmail with correct values', () => {
     const sut = new EmailValidator()
@@ -8,17 +14,22 @@ describe('EmailValidator', () => {
     sut.isValid('any_mail@mail.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any_mail@mail.com')
   })
-  it('Should throw if isEmail throws', async () => {
+  it('Should throw if isEmail throws', () => {
     const sut = new EmailValidator()
     jest.spyOn(validator, 'isEmail').mockImplementationOnce(() => {
       throw new Error()
     })
     expect(sut.isValid).toThrow()
   })
-  it('Should return false if isEmail fails', async () => {
+  it('Should return false if isEmail fails', () => {
     const sut = new EmailValidator()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const result = sut.isValid('invalid_mail')
     expect(result).toBeFalsy()
+  })
+  it('Should return true if isEmail succeed', () => {
+    const sut = new EmailValidator()
+    const result = sut.isValid('valid_mail@mail.com')
+    expect(result).toBeTruthy()
   })
 })
