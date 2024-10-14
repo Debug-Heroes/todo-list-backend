c4f5d0da84b6d9b950a44254ef756ee136013fd9
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-14 09:00:27 -0300

    test: ensure SignUpController call validation with correct values

---
+++ src/presentation/controller/signup/signup-controller.spec.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { IValidation } from '../../../domain/protocols/validation'
+import { SignUpController } from './signup-controller'
+import { HttpRequest, badRequest } from './signup-controller-protocols'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_mail@mail.com',
+    password: 'any_password',
+    confirmPassword: 'any_password'
+  }
+})
+
+interface SutTypes {
+  sut: SignUpController
+  validationStub: IValidation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub()
+  const sut = new SignUpController(validationStub)
+  return {
+    sut,
+    validationStub
+  }
+}
+
+const makeValidationStub = (): IValidation => {
+  class ValidationStub implements IValidation {
+    validate(data: any): Error | null {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+describe('SignUpController', () => {
+  it('Should call validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validationSpy = jest.spyOn(validationStub, 'validate')
+    await sut.handle(makeFakeRequest())
+    expect(validationSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+})
