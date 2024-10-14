0215c3b7dc6763cbf737fd77d6e192f76c07a4c9
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-14 12:21:07 -0300

    feat: ensure SignUpValidation call ValidationComposite with correct values

--- src/main/factory/signup/signup-validation.ts
+++ src/main/factory/signup/signup-validation.ts
@@ -1,11 +1,16 @@
 import { IValidation } from '../../../domain/protocols/validation'
+import { CompareFieldsValidation } from '../../../presentation/validations/compare-fields-validation'
+import { EmailValidation } from '../../../presentation/validations/email-validation'
 import { RequiredFieldValidation } from '../../../presentation/validations/required-field-validation'
 import { ValidationComposite } from '../../../presentation/validations/validation-composite'
+import { EmailValidator } from '../../../utils/email-validator/email-validator'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validators: IValidation[] = []
   for (const pos of ['name', 'email', 'password', 'confirmPassword']) {
     validators.push(new RequiredFieldValidation(pos))
   }
+  validators.push(new EmailValidation(new EmailValidator()))
+  validators.push(new CompareFieldsValidation('password', 'confirmPassword'))
   return new ValidationComposite(validators)
 }
