44be0f95bd69f31ca50326fe9ecb5c2f598973ae
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-14 12:14:10 -0300

    test: ensure CompareFieldsValidation return an error if comparison fails

---
+++ src/presentation/validations/compare-fields-validation.spec.ts
@@ -0,0 +1,10 @@
+import { InvalidParamError } from "../errors/invalid-param-error"
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+describe('CompareFieldsValidation', () => {
+  it('Should return an error if comparison fails', () => {
+    const sut = new CompareFieldsValidation('any_field', 'second_field')
+    const result = sut.validate({ any_field: 'any_value', second_field: 'other_value' })
+    expect(result).toEqual(new InvalidParamError('second_field'))
+  })
+})
