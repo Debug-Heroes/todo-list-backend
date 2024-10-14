3fddde5f10a0c1c8c8962273583522a386d21ac6
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-14 11:12:41 -0300

    feat: ensure RequiredFieldParam return an MissingParamError if field not found

---
+++ src/presentation/validations/required-field-param.ts
@@ -0,0 +1,13 @@
+
+import { IValidation } from "../../domain/protocols/validation";
+import { MissingParamError } from "../errors/missing-param-error";
+
+export class RequiredFieldParam implements IValidation {
+  constructor(private readonly field: string) {}
+  validate(data: any): Error | null {
+    if (!data[this.field]) {
+      return new MissingParamError(this.field)
+    }
+    return null
+  }
+}
