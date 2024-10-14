4641e5a0c4273820ff65f7500b7279b3e378c950
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-14 12:26:22 -0300

    feat: ensure ValidationComposite return the first error if more than one validation fails

--- src/presentation/validations/validation-composite.ts
+++ src/presentation/validations/validation-composite.ts
@@ -4,10 +4,12 @@ import { IValidation } from '../../domain/protocols/validation'
 export class ValidationComposite implements IValidation {
   constructor(private readonly validators: IValidation[]) {}
   validate(data: any): Error | null {
-    let error
-    this.validators.forEach((validator) => {
-      error = validator.validate(data)
-    })
-    return error ? error : null
+    for (const validator of this.validators) {
+      const error = validator.validate(data)
+      if (error) {
+        return error
+      }
+    }
+    return null
   }
 }
