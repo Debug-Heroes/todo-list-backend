249b0b9d7464f74ed2acded67fc507ed0640b167
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-14 12:18:49 -0300

    test: ensure CompareFieldsValidation not return if comparison succeed

--- src/presentation/validations/compare-fields-validation.spec.ts
+++ src/presentation/validations/compare-fields-validation.spec.ts
@@ -7,4 +7,9 @@ describe('CompareFieldsValidation', () => {
     const result = sut.validate({ any_field: 'any_value', second_field: 'other_value' })
     expect(result).toEqual(new InvalidParamError('second_field'))
   })
+  it('Should not return if comparison succeed', () => {
+    const sut = new CompareFieldsValidation('any_field', 'second_field')
+    const result = sut.validate({ any_field: 'any_value', second_field: 'any_value' })
+    expect(result).toBeFalsy()
+  })
 })
