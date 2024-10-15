19a2f30fbccb3369b886bf1628b06e21a213c810
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-15 11:51:25 -0300

    feat: ensure PgAccountRepository load method call query with correct values

--- src/infra/usecases/db/postgres/account/pg-account-repository.ts
+++ src/infra/usecases/db/postgres/account/pg-account-repository.ts
@@ -1,12 +1,18 @@
 import { IAddAccountRepository } from "../../../../../data/protocols/add-account-repository";
 import { IAccount } from "../../../../../domain/protocols/account";
 import { IAddAccountModel } from "../../../../../domain/usecases/users/add-account";
+import { ILoadAccountByEmail } from "../../../../../domain/usecases/users/load-account";
 import { PgHelper } from "../helpers/pg-helper";
 
-export class PgAccountRepository implements IAddAccountRepository {
+export class PgAccountRepository implements IAddAccountRepository, ILoadAccountByEmail {
   async add(account: IAddAccountModel): Promise<IAccount> {
     const newUser = await PgHelper.query('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *', [account.name, account.email, account.password])
     console.log(newUser.rows)
     return newUser.rows[0]
+  }
+
+  async load(email: string): Promise<IAccount | null> {
+    await PgHelper.query('SELECT * FROM users WHERE email = $1', [email])
+    return null
   }
 }
