2cfd41278013699e0e218c73f74c248c3ea2193d
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-15 09:48:08 -0300

    test: ensure access-control-allow-origin to be *

---
+++ src/main/middlewares/cors/cors.test.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express'
+import app from '../../config/app'
+import request from 'supertest'
+
+describe('cors', () => {
+  it('Should access-control-allow-origin to be *', async () => {
+    app.get('/test_cors', (req: Request, res: Response) => {
+      res.send(req.body)
+    })
+
+    const response = await request(app)
+      .get('/test_cors')
+      .set('Origin', 'https://anyurlfrominternet.com')
+    expect(response.statusCode).toBe(200)
+    expect(response.headers['access-control-allow-origin']).toBe('*')
+  })
+})
