500f7508537c68f3c8ef3e13ffb08fd3c8826f76
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-15 06:23:37 -0300

    feat: ensure DbAddAccount call encrypter with correct values

---
+++ src/data/usecases/db/db-add-account.ts
@@ -0,0 +1,21 @@
+ 
+import { IAccount } from "../../../domain/protocols/account";
+import { IAddAccount, IAddAccountModel } from "../../../domain/usecases/users/add-account";
+import { IAddAccountRepository } from "../../protocols/add-account-repository";
+import { IEncrypter } from "../../protocols/encrypter";
+
+export class DbAddAccount implements IAddAccount {
+  constructor(
+    private readonly hasher: IEncrypter,
+    private readonly addAccountRepository: IAddAccountRepository
+  ) {}
+  async add(account: IAddAccountModel): Promise<IAccount> {
+    await this.hasher.encrypt(account.password)
+    return Promise.resolve({
+      email: '',
+      id: '',
+      name: '',
+      password: ''
+    })
+  }
+}
--- src/domain/usecases/users/add-account.ts
+++ src/domain/usecases/users/add-account.ts
@@ -1,7 +1,7 @@
 import { IAccount } from "../../protocols/account"
 
 export interface IAddAccount {
-  add (account: AddAccountModel): Promise<IAccount>
+  add (account: IAddAccountModel): Promise<IAccount>
 }
 
 export interface IAddAccountModel {
