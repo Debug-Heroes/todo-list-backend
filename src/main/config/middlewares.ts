f28219801ad84393dd84f702d4b5a31a6ddf1ade
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-15 09:56:42 -0300

    feat: ensure Morgan log the request information on console

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from "express"
 import bodyParser from "../middlewares/body-parser/body-parser"
 import cors from "../middlewares/cors/cors"
+import morgan from "../middlewares/morgan/morgan"
 
 export default (app: Express) => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(morgan)
 }
