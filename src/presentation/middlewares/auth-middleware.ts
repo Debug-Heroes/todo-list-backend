3150ae3c9b37891c0bb3aa5d4147048c142023a3
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-15 10:34:39 -0300

    feat: ensure AuthMiddleware return 500 if decrypter throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,18 +1,21 @@
-import { forbidden } from "../helpers/http-helper";
-import { Controller } from "../protocols/controller";
-import { IDecrypter } from "../../domain/usecases/cryptography/decrypter";
-import { HttpRequest, HttpResponse } from "../protocols/http";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { forbidden, serverError } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
+import { IDecrypter } from '../../domain/usecases/cryptography/decrypter'
+import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class AuthMiddleware implements Controller {
-  constructor(
-    private readonly decrypter: IDecrypter
-  ) {}
+  constructor(private readonly decrypter: IDecrypter) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.headers?.authorization) {
-      return Promise.resolve(forbidden())
-    }
+    try {
+      if (!httpRequest.headers?.authorization) {
+        return Promise.resolve(forbidden())
+      }
 
-    await this.decrypter.decrypt(httpRequest.headers.authorization)
-    return Promise.resolve({statusCode: 200})
+      await this.decrypter.decrypt(httpRequest.headers.authorization)
+      return Promise.resolve({ statusCode: 200 })
+    } catch (error: any) {
+      return new Promise((resolve) => resolve(serverError()))
+    }
   }
 }
