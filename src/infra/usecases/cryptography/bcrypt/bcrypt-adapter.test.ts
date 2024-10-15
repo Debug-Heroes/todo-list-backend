d0926ca81ac2a681da7499d27176b29e9afa4f18
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-15 07:29:14 -0300

    test: ensure BcryptAdapter call hash with correct values

---
+++ src/infra/usecases/cryptography/bcrypt/bcrypt-adapter.test.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+const salt = 12
+const makeSut = (): BcryptAdapter => {
+  const sut = new BcryptAdapter(salt)
+  return sut
+}
+describe('BcryptAdapter', () => {
+  it('Should call hash with correct values', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+})
