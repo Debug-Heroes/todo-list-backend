37a6ba27c14bf0f8203988c0493f68bb625e720f
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-16 07:26:33 -0300

    feat: ensure MaxLengthValidation return an CharactersLimitReached if field exceeded the limit

---
+++ src/presentation/validations/max-length-validation.ts
@@ -0,0 +1,14 @@
+import { IValidation } from '../../domain/usecases/users/validation'
+import { CharactersLimitReached } from '../errors/characters-limit-reached-error'
+
+export class MaxLengthValidation implements IValidation {
+  constructor(
+    private readonly field: string,
+    private readonly limit: number
+  ) {}
+  validate(data: any): Error | null {
+    if (data[this.field].length > this.limit) 
+      return new CharactersLimitReached()
+    return null
+  }
+}
