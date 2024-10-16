56571f0c34404df4b946cb0e8698ab52d079d846
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-16 09:51:23 -0300

    feat: ensure DbAuthentication not return if loadByEmail returns null

--- src/data/usecases/db/authenticate/db-authenticate.ts
+++ src/data/usecases/db/authenticate/db-authenticate.ts
@@ -5,7 +5,10 @@ import { ILoadAccountByEmail } from "../../../../domain/usecases/users/load-acco
 export class DbAuthentication implements IAuthentication {
   constructor(private readonly loadByEmail: ILoadAccountByEmail) {}
   async auth(account: IAuthenticationModel): Promise<IAccount | null> {
-    await this.loadByEmail.load(account.email)
-    return Promise.resolve(null)
+    const foundAccount = await this.loadByEmail.load(account.email)
+    if (!foundAccount) {
+      return Promise.resolve(null)
+    }
+    return Promise.resolve({} as IAccount)
   }
 }
