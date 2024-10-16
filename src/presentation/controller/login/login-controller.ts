69de7c3c21dfae2934b5ef64e539b6d896407ac2
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-16 08:19:19 -0300

    feat: ensure LoginController call encrypter with correct values

--- src/presentation/controller/login/login-controller.ts
+++ src/presentation/controller/login/login-controller.ts
@@ -1,3 +1,4 @@
+import { IEncrypter } from "../../../data/protocols/encrypter";
 import { IAuthentication } from "../../../domain/usecases/users/authentication";
 import { IValidation } from "../../../domain/usecases/users/validation";
 import { badRequest, Controller, HttpRequest, HttpResponse, unauthorized } from "../signup/signup-controller-protocols";
@@ -5,7 +6,8 @@ import { badRequest, Controller, HttpRequest, HttpResponse, unauthorized } from 
 export class LoginController implements Controller {
   constructor(
     private readonly validation: IValidation,
-    private readonly authenticator: IAuthentication
+    private readonly authenticator: IAuthentication,
+    private readonly encrypter: IEncrypter
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
@@ -16,6 +18,7 @@ export class LoginController implements Controller {
     if (!user) {
       return new Promise(resolve => resolve(unauthorized()))
     }
+    await this.encrypter.encrypt(user.id)
     return new Promise(resolve => resolve({statusCode: 200}))
   }
 }
