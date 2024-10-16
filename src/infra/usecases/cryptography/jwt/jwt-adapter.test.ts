baf51ed04af481b7e6994ca3f75272a475ce4b0d
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-16 11:02:20 -0300

    test: ensure JwtAdapter call sign with correct values

--- src/infra/usecases/cryptography/jwt/jwt-adapter.test.ts
+++ src/infra/usecases/cryptography/jwt/jwt-adapter.test.ts
@@ -16,21 +16,31 @@ jest.mock('jsonwebtoken', () => ({
 }))
 
 describe('JwtAdapter', () => {
-  it('Should call verify with correct values', async () => {
-    const sut = makeSut()
-    const verifySpy = jest.spyOn(jwt, 'verify')
-    await sut.decrypt('any_value')
-    expect(verifySpy).toHaveBeenCalledWith('any_value', 'any_secret')
+  describe('decrypt', () => {
+    it('Should call verify with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_value')
+      expect(verifySpy).toHaveBeenCalledWith('any_value', 'any_secret')
+    })
+    it('Should return null on verify fails', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockReturnValueOnce('any_error' as any)
+      const result = await sut.decrypt('any_value')
+      expect(result).toBeFalsy()
+    })
+    it('Should return an id on verify succeed', async () => {
+      const sut = makeSut()
+      const result = await sut.decrypt('any_value')
+      expect(result).toBe('any_id')
+    })
   })
-  it('Should return null on verify fails', async () => {
-    const sut = makeSut()
-    jest.spyOn(jwt, 'verify').mockReturnValueOnce('any_error' as any)
-    const result = await sut.decrypt('any_value')
-    expect(result).toBeFalsy()
-  })
-  it('Should return an id on verify succeed', async () => {
-    const sut = makeSut()
-    const result = await sut.decrypt('any_value')
-    expect(result).toBe('any_id')
+  describe('encrypt', () => {
+    it('Should call sign with correct values', async () => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt('any_value')
+      expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'any_secret', { expiresIn: '8h' })
+    })
   })
 })
