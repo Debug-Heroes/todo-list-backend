0bc7e15ab3496af22775b483e6a55fa81bf9d3d7
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-16 09:30:53 -0300

    test: ensure LoginValidation call ValidationComposite with correct values

---
+++ src/main/factory/login/login-validation.spec.ts
@@ -0,0 +1,20 @@
+import { IValidation } from "../../../domain/usecases/users/validation"
+import { ValidationComposite } from "../../../presentation/validations/validation-composite"
+import { RequiredFieldValidation } from '../../../presentation/validations/required-field-validation'
+import { makeLoginValidation } from './login-validation'
+import { EmailValidation } from "../../../presentation/validations/email-validation"
+import { EmailValidator } from "../../../utils/email-validator/email-validator"
+
+jest.mock("../../../presentation/validations/validation-composite")
+
+describe('LoginValidation', () => {
+  it('Should call  ValidationComposite with correct values', async () => {
+    makeLoginValidation()
+    const validations: IValidation[] = []
+    for (const pos of ['email', 'password']) {
+      validations.push(new RequiredFieldValidation(pos))
+    }
+    validations.push(new EmailValidation(new EmailValidator()))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
