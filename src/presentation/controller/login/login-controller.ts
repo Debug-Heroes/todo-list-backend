79a342f19637709d95772f791f8dcebc5144a07f
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-16 08:23:21 -0300

    feat: ensure LoginController return 500 if authenticator throws

--- src/presentation/controller/login/login-controller.ts
+++ src/presentation/controller/login/login-controller.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { IEncrypter } from "../../../data/protocols/encrypter";
 import { IAuthentication } from "../../../domain/usecases/users/authentication";
 import { IValidation } from "../../../domain/usecases/users/validation";
-import { badRequest, Controller, HttpRequest, HttpResponse, unauthorized } from "../signup/signup-controller-protocols";
+import { badRequest, Controller, HttpRequest, HttpResponse, serverError, unauthorized } from "../signup/signup-controller-protocols";
 
 export class LoginController implements Controller {
   constructor(
@@ -10,15 +11,19 @@ export class LoginController implements Controller {
     private readonly encrypter: IEncrypter
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return new Promise(resolve => resolve(badRequest(error)))
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return new Promise(resolve => resolve(badRequest(error)))
+      }
+      const user = await this.authenticator.auth(httpRequest.body)
+      if (!user) {
+        return new Promise(resolve => resolve(unauthorized()))
+      }
+      await this.encrypter.encrypt(user.id)
+      return new Promise(resolve => resolve({statusCode: 200}))
+    } catch (error: any) {
+      return new Promise(resolve => resolve(serverError()))
     }
-    const user = await this.authenticator.auth(httpRequest.body)
-    if (!user) {
-      return new Promise(resolve => resolve(unauthorized()))
-    }
-    await this.encrypter.encrypt(user.id)
-    return new Promise(resolve => resolve({statusCode: 200}))
   }
 }
