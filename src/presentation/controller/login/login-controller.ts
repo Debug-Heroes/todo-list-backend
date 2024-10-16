fea2513cb7f17a705dfe4aa637ccf1cfcc36396a
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-16 07:51:47 -0300

    feat: ensure LoginController return a 400 if validation fails

--- src/presentation/controller/login/login-controller.ts
+++ src/presentation/controller/login/login-controller.ts
@@ -1,10 +1,13 @@
 import { IValidation } from "../../../domain/usecases/users/validation";
-import { Controller, HttpRequest, HttpResponse } from "../signup/signup-controller-protocols";
+import { badRequest, Controller, HttpRequest, HttpResponse } from "../signup/signup-controller-protocols";
 
 export class LoginController implements Controller {
   constructor(private readonly validation: IValidation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body) 
-    return Promise.resolve({statusCode: 200})
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return new Promise(resolve => resolve(badRequest(error)))
+    }
+    return new Promise(resolve => resolve({statusCode: 200}))
   }
 }
