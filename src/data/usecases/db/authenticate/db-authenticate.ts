07f20e482b07fcd130e87f818ad3303c3e7a301e
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-16 10:07:32 -0300

    feat: ensure DbAuthentication call comparer with correct value

--- src/data/usecases/db/authenticate/db-authenticate.ts
+++ src/data/usecases/db/authenticate/db-authenticate.ts
@@ -1,14 +1,19 @@
 import { IAccount } from "../../../../domain/protocols/account";
 import { IAuthentication, IAuthenticationModel } from "../../../../domain/usecases/users/authentication";
 import { ILoadAccountByEmail } from "../../../../domain/usecases/users/load-account";
+import { IComparer } from "../../../protocols/criptography/comparer";
 
 export class DbAuthentication implements IAuthentication {
-  constructor(private readonly loadByEmail: ILoadAccountByEmail) {}
+  constructor(
+    private readonly loadByEmail: ILoadAccountByEmail,
+    private readonly comparer: IComparer
+  ) {}
   async auth(account: IAuthenticationModel): Promise<IAccount | null> {
     const foundAccount = await this.loadByEmail.load(account.email)
     if (!foundAccount) {
       return Promise.resolve(null)
     }
+    await this.comparer.compare(account.password, foundAccount.password)
     return Promise.resolve({} as IAccount)
   }
 }
