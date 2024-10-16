9e53efdc09f29989c1d605edd5bff178db8c279f
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-16 11:28:26 -0300

    feat: move login controller creation to a factory

---
+++ src/main/factory/login/login-controller.ts
@@ -0,0 +1,17 @@
+import { DbAuthentication } from "../../../data/usecases/db/authenticate/db-authenticate"
+import { BcryptAdapter } from "../../../infra/usecases/cryptography/bcrypt/bcrypt-adapter"
+import { JwtAdapter } from "../../../infra/usecases/cryptography/jwt/jwt-adapter"
+import { PgAccountRepository } from "../../../infra/usecases/db/postgres/account/pg-account-repository"
+import { LoginController } from "../../../presentation/controller/login/login-controller"
+import env from "../../config/env"
+import { makeLoginValidation } from "./login-validation"
+
+export const makeLoginController = (): LoginController => {
+  const salt = 12
+  const loadByEmail = new PgAccountRepository()
+  const comparer = new BcryptAdapter(salt)
+  const encrypter = new JwtAdapter(env.JWT_SECRET)
+  const authenticator = new DbAuthentication(loadByEmail, comparer, encrypter)
+  const loginController = new LoginController(makeLoginValidation(), authenticator)
+  return loginController
+}
