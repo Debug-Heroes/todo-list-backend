cde4d4c5da45d78b755d4cb47c6d10b763dbbce7
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-17 14:24:04 -0300

    refactor: change auth middleware apply method

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,12 +1,9 @@
 import express from 'express'
 import middlewares from './middlewares'
 import routes from './routes'
-import middlewareAdapter from '../adapters/middleware-adapter'
-import { makeAuthMiddleware } from '../factory/auth-middleware/auth-middleware-factory'
 const app = express()
 
 middlewares(app)
 routes(app)
-app.use(middlewareAdapter(makeAuthMiddleware()) as any)
 
 export default app
