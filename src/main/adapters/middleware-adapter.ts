c2d34391c8c3838d0e4abbeea290918dc5f9b04d
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-17 14:26:47 -0300

    fix: express conflicting with route and middleware adapter

--- src/main/adapters/middleware-adapter.ts
+++ src/main/adapters/middleware-adapter.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import { Controller } from '../../presentation/protocols/controller'
 
 export default (middleware: Controller) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
+  return async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     const response = await middleware.handle(req.body)
     if (response.statusCode === 200) return next()
     return res.status(response.statusCode).json(response.body)
