1d18902480174945ef1eea472c928a8162e76efd
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-18 10:45:28 -0300

    test: ensure DeleteUserController call loadAccountById with correct values

--- src/presentation/controller/user/delete-user/delete-user-controller.spec.ts
+++ src/presentation/controller/user/delete-user/delete-user-controller.spec.ts
@@ -3,18 +3,23 @@ import { HttpRequest } from '../../../protocols/http'
 import { IValidation } from '../../../protocols/validation'
 import { badRequest } from '../../signup/signup-controller-protocols'
 import { DeleteUserController } from './delete-user-controller'
+import { ILoadAccountById } from '../../../../domain/usecases/users/load-account-by-id'
+import { IAccount } from '../../../../domain/protocols/account'
 
 interface SutTypes {
   sut: DeleteUserController
   validationStub: IValidation
+  loadUserByIdStub: ILoadAccountById
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = makeValidationStub()
-  const sut = new DeleteUserController(validationStub)
+  const loadUserByIdStub = makeLoadUserStub()
+  const sut = new DeleteUserController(validationStub, loadUserByIdStub)
   return {
     sut,
-    validationStub
+    validationStub,
+    loadUserByIdStub
   }
 }
 
@@ -25,6 +30,20 @@ const makeValidationStub = (): IValidation => {
     }
   }
   return new ValidationStub()
+}
+
+const makeLoadUserStub = (): ILoadAccountById => {
+  class LoadUserByIdStub implements ILoadAccountById {
+    async load(id: string): Promise<IAccount | null> {
+      return new Promise(resolve => resolve({
+        email: 'any_mail@mail.com',
+        password: 'any_hash',
+        id: 'any_id',
+        name: 'any_name'
+      }))
+    }
+  }
+  return new LoadUserByIdStub()
 }
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -45,5 +64,11 @@ describe('DeleteUserController', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error('any_error'))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(badRequest(new Error('any_error')))
+  })
+  it('Should call loadUserById with correct values', async () => {
+    const { sut, loadUserByIdStub } = makeSut()
+    const loadSpy = jest.spyOn(loadUserByIdStub, 'load')
+    await sut.handle(makeFakeRequest())
+    expect(loadSpy).toHaveBeenCalledWith('any_id')
   })
 })
