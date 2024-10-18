39955e4a8bea1dc878c1b0d2a04a781d66979057
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-18 11:36:40 -0300

    feat: ensure DeleteUserController call deleteAccount with correct values

--- src/presentation/controller/user/delete-user/delete-user-controller.ts
+++ src/presentation/controller/user/delete-user/delete-user-controller.ts
@@ -1,3 +1,4 @@
+import { IDeleteAccount } from "../../../../domain/usecases/users/delete-account";
 import { ILoadAccountById } from "../../../../domain/usecases/users/load-account-by-id";
 import { IValidation } from "../../../protocols/validation";
 import { badRequest, Controller, HttpRequest, HttpResponse, NotFound, ok, serverError } from "../../login/login-controller-protocols";
@@ -5,24 +6,24 @@ import { badRequest, Controller, HttpRequest, HttpResponse, NotFound, ok, server
 export class DeleteUserController implements Controller {
   constructor (
     private readonly validation: IValidation,
-    private readonly loadAccountById: ILoadAccountById
+    private readonly loadAccountById: ILoadAccountById,
+    private readonly deleteAccount: IDeleteAccount
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      
       // validar se tem id >> 400
       const error = this.validation.validate(httpRequest.body)
       if (error) {
         return new Promise(resolve => resolve(badRequest(error)))
       }
       const { id } = httpRequest.body
+      // Verificar se usuario existe 404 || 500
       const user = await this.loadAccountById.load(id)
       if (!user) {
         return new Promise(resolve => resolve(NotFound()))
       }
-  
-      // Verificar se usuario existe 404 || 500
       // deletar usuario >> 200 || 500
+      await this.deleteAccount.delete(id)
       return new Promise(resolve => resolve(ok('')))
     } catch (error) {
       return new Promise(resolve => resolve(serverError()))
