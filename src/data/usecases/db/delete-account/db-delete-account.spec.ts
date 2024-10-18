b8922e8d6927c77d5769a84d0852f2d2b42d115e
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-18 13:26:18 -0300

    test: ensure DbDeleteAccount call repository with correct value

---
+++ src/data/usecases/db/delete-account/db-delete-account.spec.ts
@@ -0,0 +1,34 @@
+import { DbDeleteAccount } from './db-delete-account'
+import { IDeleteAccountRepository } from '../../../protocols/db/delete-account-repository'
+
+interface SutTypes {
+  sut: DbDeleteAccount
+  deleteAccountRepositoryStub: IDeleteAccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const deleteAccountRepositoryStub = makeDeleteAccountRepositoryStub()
+  const sut = new DbDeleteAccount(deleteAccountRepositoryStub)
+  return {
+    sut,
+    deleteAccountRepositoryStub
+  }
+}
+
+const makeDeleteAccountRepositoryStub = (): IDeleteAccountRepository => {
+  class DeleteAccountRepositoryStub implements IDeleteAccountRepository {
+    delete(id: string): Promise<string> {
+      return Promise.resolve('affected rows 1')
+    }
+  }
+  return new DeleteAccountRepositoryStub()
+}
+
+describe('DbDeleteAccount', () => {
+  it('Should call repository with correct value', async () => {
+    const { sut, deleteAccountRepositoryStub } = makeSut() 
+    const deleteSpy = jest.spyOn(deleteAccountRepositoryStub, 'delete')
+    await sut.delete('any_id')
+    expect(deleteSpy).toHaveBeenCalledWith('any_id')
+  })
+})
