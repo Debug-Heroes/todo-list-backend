f04c42d02c1902eae1e08ce30e6c2309c0365d2e
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-18 10:42:18 -0300

    feat: ensure DeleteUserController call loadAccountById with correct values

--- src/presentation/controller/user/delete-user/delete-user-controller.ts
+++ src/presentation/controller/user/delete-user/delete-user-controller.ts
@@ -1,9 +1,11 @@
+import { ILoadAccountById } from "../../../../domain/usecases/users/load-account-by-id";
 import { IValidation } from "../../../protocols/validation";
 import { badRequest, Controller, HttpRequest, HttpResponse, ok } from "../../login/login-controller-protocols";
 
 export class DeleteUserController implements Controller {
   constructor (
-    private readonly validation: IValidation
+    private readonly validation: IValidation,
+    private readonly loadAccountById: ILoadAccountById
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     // validar se tem id >> 400
@@ -11,6 +13,8 @@ export class DeleteUserController implements Controller {
     if (error) {
       return new Promise(resolve => resolve(badRequest(error)))
     }
+    const { id } = httpRequest.body
+    await this.loadAccountById.load(id)
     // verificar se usuario existe >> 404 || 500
     // deletar usuario >> 200 || 500
     return new Promise(resolve => resolve(ok('')))
