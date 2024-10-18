55b049f1e9cad9f4063fc2994b9756722fcd094b
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-18 11:02:44 -0300

    feat: ensure DeleteUserController return 500 if LoadAccountById throws

--- src/presentation/controller/user/delete-user/delete-user-controller.ts
+++ src/presentation/controller/user/delete-user/delete-user-controller.ts
@@ -1,6 +1,6 @@
 import { ILoadAccountById } from "../../../../domain/usecases/users/load-account-by-id";
 import { IValidation } from "../../../protocols/validation";
-import { badRequest, Controller, HttpRequest, HttpResponse, NotFound, ok } from "../../login/login-controller-protocols";
+import { badRequest, Controller, HttpRequest, HttpResponse, NotFound, ok, serverError } from "../../login/login-controller-protocols";
 
 export class DeleteUserController implements Controller {
   constructor (
@@ -8,18 +8,24 @@ export class DeleteUserController implements Controller {
     private readonly loadAccountById: ILoadAccountById
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    // validar se tem id >> 400
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return new Promise(resolve => resolve(badRequest(error)))
+    try {
+      
+      // validar se tem id >> 400
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return new Promise(resolve => resolve(badRequest(error)))
+      }
+      const { id } = httpRequest.body
+      const user = await this.loadAccountById.load(id)
+      if (!user) {
+        return new Promise(resolve => resolve(NotFound()))
+      }
+  
+      // Verificar se usuario existe 404 || 500
+      // deletar usuario >> 200 || 500
+      return new Promise(resolve => resolve(ok('')))
+    } catch (error) {
+      return new Promise(resolve => resolve(serverError()))
     }
-    const { id } = httpRequest.body
-    const user = await this.loadAccountById.load(id)
-    if (!user) {
-      return new Promise(resolve => resolve(NotFound()))
-    }
-    // verificar se usuario existe >> 404 || 500
-    // deletar usuario >> 200 || 500
-    return new Promise(resolve => resolve(ok('')))
   }
 }
