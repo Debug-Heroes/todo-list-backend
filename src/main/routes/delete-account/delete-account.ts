4080bc472728cf750727cad163925d14433a0a72
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-21 08:59:20 -0300

    feat: auth middleware add to delete user route

--- src/main/routes/delete-account/delete-account.ts
+++ src/main/routes/delete-account/delete-account.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import routeAdapter from "../../adapters/route-adapter";
 import { makeDeleteAccount } from "../../factory/users/delete-account-factory";
+import middlewareAdapter from "../../adapters/middleware-adapter";
+import { makeAuthMiddleware } from "../../factory/auth-middleware/auth-middleware-factory";
 
 export default (router: Router) => {
-  router.delete('/user', routeAdapter(makeDeleteAccount()))
+  router.delete('/user', middlewareAdapter(makeAuthMiddleware()), routeAdapter(makeDeleteAccount()))
 }
