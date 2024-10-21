6488c724f6a3b819f449eb3cbd62b7147145433f
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-21 08:05:54 -0300

    feat: ensure delete account route return 200 on succeed

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,7 +4,7 @@ import fs from 'fs'
 export default (app: Express) => {
   const router = Router()
 
-  for (const route of ['signup', 'login']) {
+  for (const route of ['signup', 'login', 'delete-account']) {
     fs.readdirSync(`${__dirname}/../routes/${route}`).map(async (file) => {
       if (!file.includes('test') && !file.includes('map')) {
         (await import(`../routes/${route}/${file}`)).default(router)
