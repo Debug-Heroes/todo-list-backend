78e0779ecf614a003e09315eb2f293418da02914
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-21 09:26:23 -0300

    refactor: formatting code

--- src/main/factory/users/delete-account-factory.ts
+++ src/main/factory/users/delete-account-factory.ts
@@ -1,8 +1,12 @@
-import { PgAccountRepository } from "../../../infra/usecases/db/postgres/account/pg-account-repository";
-import { DeleteUserController } from "../../../presentation/controller/user/delete-user/delete-user-controller";
-import { makeDeleteValidation } from "./delete-validation";
+import { PgAccountRepository } from '../../../infra/usecases/db/postgres/account/pg-account-repository'
+import { DeleteUserController } from '../../../presentation/controller/user/delete-user/delete-user-controller'
+import { makeDeleteValidation } from './delete-validation'
 
 export const makeDeleteAccount = (): DeleteUserController => {
-  const deleteUserController = new DeleteUserController(makeDeleteValidation(), new PgAccountRepository(), new PgAccountRepository())
+  const deleteUserController = new DeleteUserController(
+    makeDeleteValidation(),
+    new PgAccountRepository(),
+    new PgAccountRepository()
+  )
   return deleteUserController
 }
