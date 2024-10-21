1153817fdf47c53fb223cd9ea5c4d8af29b8582d
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-21 06:03:05 -0300

    feat: ensure PgAccountRepository call query with correct values

--- src/infra/usecases/db/postgres/account/pg-account-repository.ts
+++ src/infra/usecases/db/postgres/account/pg-account-repository.ts
@@ -1,11 +1,12 @@
 import { IAddAccountRepository } from '../../../../../data/protocols/db/add-account-repository'
+import { IDeleteAccountRepository } from '../../../../../data/protocols/db/delete-account-repository'
 import { IAccount } from '../../../../../domain/protocols/account'
 import { IAddAccountModel } from '../../../../../domain/usecases/users/add-account'
 import { ILoadAccountByEmail } from '../../../../../domain/usecases/users/load-account'
 import { PgHelper } from '../helpers/pg-helper'
 
 export class PgAccountRepository
-  implements IAddAccountRepository, ILoadAccountByEmail
+  implements IAddAccountRepository, ILoadAccountByEmail, IDeleteAccountRepository
 {
   async add(account: IAddAccountModel): Promise<IAccount> {
     const newUser = await PgHelper.query(
@@ -21,5 +22,10 @@ export class PgAccountRepository
       [email]
     )
     return result.rows?.length > 0 ? result.rows[0] : null
+  }
+
+  async delete(id: string): Promise<string> {
+    await PgHelper.query('DELETE FROM users WHERE id = $1', [id])
+    return Promise.resolve('')
   }
 }
