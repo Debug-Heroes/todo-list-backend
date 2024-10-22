0d62ebbd4c81bb7ca7962add8e2104c32d0fd8c2
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-22 16:50:20 -0300

    test: ensure get all categories route return 200 on succeed

---
+++ src/main/routes/categories/get-all-categories.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest'
+import app from '../../config/app'
+import { NextFunction } from 'express'
+import { PgHelper } from '../../../infra/usecases/db/postgres/helpers/pg-helper'
+import { TestPoolConfig } from '../../../infra/usecases/db/postgres/test/pg-pool-config'
+
+jest.mock('../../adapters/middleware-adapter', () => () => async (req: Request, res: Response, next: NextFunction) => next())
+
+describe('GetAllCategoriesRoutes', () => {
+  beforeAll(async () => {
+    PgHelper.connect(TestPoolConfig).then(() => {})
+  })
+  afterAll(async () => {
+    PgHelper.disconnect().then(() => {})
+  })
+  it('Should return 200 on succeed', async () => {
+    await request(app)
+      .get('/api/categories')
+      .expect(200)
+  })
+})
