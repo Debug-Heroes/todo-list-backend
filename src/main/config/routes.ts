0b7dfcbe35e49dbf139cb43890f91f6d42963666
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-22 16:49:45 -0300

    feat: ensure get all categories route return 200 on succeed

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -2,15 +2,25 @@ import { Express, Router } from 'express'
 import fs from 'fs'
 
 export default (app: Express) => {
-  const router = Router()
+  const authRoutes = Router()
+  const apiRoutes = Router()
 
   for (const route of ['signup', 'login', 'delete-account']) {
     fs.readdirSync(`${__dirname}/../routes/users/${route}`).map(async (file) => {
       if (!file.includes('test') && !file.includes('map')) {
-        (await import(`../routes/users/${route}/${file}`)).default(router)
+        (await import(`../routes/${route}/users/${file}`)).default(authRoutes)
       }
     })
   }
 
-  app.use('/', router)
+  for (const route of ['categories']) {
+    fs.readdirSync(`${__dirname}/../routes/${route}`).map(async (file) => {
+      if (!file.includes('test') && !file.includes('map')) {
+        (await import(`../routes/${route}/${file}`)).default(apiRoutes)
+      }
+    })
+  }
+
+  app.use('/', authRoutes)
+  app.use('/api', apiRoutes)
 }
---
+++ src/main/routes/categories/get-all-categories.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import routeAdapter from "../../adapters/route-adapter";
+import middlewareAdapter from "../../adapters/middleware-adapter";
+import { makeAuthMiddleware } from "../../factory/auth-middleware/auth-middleware-factory";
+import { makeGetAllCategoriesController } from "../../factory/categories/get-all-categories-factory";
+
+export default (router: Router) => {
+  router.get('/categories', middlewareAdapter(makeAuthMiddleware()), routeAdapter(makeGetAllCategoriesController()))
+}
