29aa8627996870297821e208bb7d753b80f02c06
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-22 16:29:47 -0300

    test: ensure PgCategoriesRepository return categories on query succeed

--- src/infra/usecases/db/postgres/repositories/categories/pg-categories-repository.test.ts
+++ src/infra/usecases/db/postgres/repositories/categories/pg-categories-repository.test.ts
@@ -34,5 +34,13 @@ describe('PgCategoriesRepository', () => {
       const promise = sut.getAll()
       expect(promise).rejects.toThrow()
     })
+    it('Should return categories on query succeed', async () => {
+      PgHelper.query('INSERT INTO categories(name) VALUES($1)', ['any_category']).then(async () => {
+        const sut = new PgCategoriesRepository()
+        const result = await sut.getAll()
+        expect(result[0].id).toBeTruthy()
+        expect(result[0].name).toBe('any_category')
+      })
+    })
   })
 })
