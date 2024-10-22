bccf0c81702a2740a129ae4b2da2a402d986edce
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-22 16:09:20 -0300

    refactor: move PgAccountRepository to repositories folder

--- src/main/factory/users/signup/signup-controller.ts
+++ src/main/factory/users/signup/signup-controller.ts
@@ -1,7 +1,7 @@
 import { DbAddAccount } from '@data/usecases/db/users/add-account/db-add-account'
-import { BcryptAdapter } from '@infra/usecases/cryptography/bcrypt/bcrypt-adapter'
+import { BcryptAdapter } from '../../../../infra/usecases/cryptography/bcrypt/bcrypt-adapter'
 import { PgAccountRepository } from '@infra/usecases/db/postgres/repositories/account/pg-account-repository'
-import { SignUpController } from '@presentation/controller/user/signup/signup-controller'
+import { SignUpController } from '../../../../presentation/controller/user/signup/signup-controller'
 import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): SignUpController => {
