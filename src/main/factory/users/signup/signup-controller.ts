84bbe1912c379b15824ab80e0d390728cca46bfc
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-22 15:46:55 -0300

    refactor: move users usecases to respective folder

--- src/main/factory/users/signup/signup-controller.ts
+++ src/main/factory/users/signup/signup-controller.ts
@@ -1,7 +1,7 @@
-import { DbAddAccount } from '../../../../data/usecases/db/users/add-account/db-add-account'
-import { BcryptAdapter } from '../../../../infra/usecases/cryptography/bcrypt/bcrypt-adapter'
-import { PgAccountRepository } from '../../../../infra/usecases/db/postgres/repositories/account/pg-account-repository'
-import { SignUpController } from '../../../../presentation/controller/user/signup/signup-controller'
+import { DbAddAccount } from '@data/usecases/db/users/add-account/db-add-account'
+import { BcryptAdapter } from '@infra/usecases/cryptography/bcrypt/bcrypt-adapter'
+import { PgAccountRepository } from '@infra/usecases/db/postgres/repositories/account/pg-account-repository'
+import { SignUpController } from '@presentation/controller/user/signup/signup-controller'
 import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): SignUpController => {
