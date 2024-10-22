a5d5a90648c1922f2d5c5f10c5bc78f0b76ac25c
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-22 16:49:45 -0300

    feat: ensure get all categories route return 200 on succeed

---
+++ src/main/factory/categories/get-all-categories-factory.ts
@@ -0,0 +1,10 @@
+import { DbGetAllCategories } from "../../../data/usecases/db/categories/get/get-all-categories";
+import { PgCategoriesRepository } from "../../../infra/usecases/db/postgres/repositories/categories/pg-categories-repository";
+import { GetAllCategoriesController } from "../../../presentation/controller/categories/get/get-all-categories-controller";
+
+export const makeGetAllCategoriesController = (): GetAllCategoriesController => {
+  const repository = new PgCategoriesRepository()
+  const dbGetAllCategories = new DbGetAllCategories(repository)
+  const getAllCategories = new GetAllCategoriesController(dbGetAllCategories)
+  return getAllCategories
+}
