a53893b493856fd3ffc59fd211aaebb6a9159cea
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-22 14:48:38 -0300

    feat: ensure GetAllCategoriesController return 500 if DbGetAllCategories throws

--- src/presentation/controller/categories/get/get-all-categories-controller.ts
+++ src/presentation/controller/categories/get/get-all-categories-controller.ts
@@ -1,11 +1,16 @@
 import { IGetAllCategories } from '../../../../domain/usecases/categories/get-all-categories'
+import { serverError } from '../../../helpers/http-helper'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 
 export class GetAllCategoriesController implements Controller {
   constructor(private readonly getAllCategories: IGetAllCategories) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.getAllCategories.getAll()
-    return Promise.resolve({ statusCode: 200 })
+    try {
+      await this.getAllCategories.getAll()
+      return Promise.resolve({ statusCode: 200 })
+    } catch (error) {
+      return new Promise(resolve => resolve(serverError()))
+    }
   }
 }
