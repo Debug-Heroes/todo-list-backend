2129f1f6ce1b6d365f0f0ea2e4923896567efdf1
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-23 09:16:19 -0300

    feat: ensure DbUpdateAccount call repository with correct values

---
+++ src/data/protocols/db/update-account-repository.ts
@@ -0,0 +1,6 @@
+import { IAccount } from "../../../domain/protocols/account";
+import { UpdateAccountModel } from "../../../domain/usecases/users/update-account";
+
+export interface IUpdateAccountRepository {
+  update(account: UpdateAccountModel): Promise<IAccount>
+}
