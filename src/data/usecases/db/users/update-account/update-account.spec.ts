f38fd071df008dd5c4d8adb386151617cb528e1c
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-23 09:20:19 -0300

    test: ensure DbUpdateAccount call repository with correct values

---
+++ src/data/usecases/db/users/update-account/update-account.spec.ts
@@ -0,0 +1,46 @@
+import { IAccount } from '../../../../../domain/protocols/account'
+import { IUpdatedAccount, UpdateAccountModel } from '../../../../../domain/usecases/users/update-account'
+import { IUpdateAccountRepository } from '../../../../protocols/db/update-account-repository'
+import { DbUpdateAccount } from './update-account'
+
+interface SutTypes {
+  sut: DbUpdateAccount
+  updateAccountRepositoryStub: IUpdateAccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const updateAccountRepositoryStub = makeUpdateAccountRepositoryStub()
+  const sut = new DbUpdateAccount(updateAccountRepositoryStub)
+  return {
+    sut,
+    updateAccountRepositoryStub
+  }
+}
+
+const makeUpdateAccountRepositoryStub = (): IUpdateAccountRepository => {
+  class UpdateAccountRepStub implements IUpdateAccountRepository {
+    async update(account: UpdateAccountModel): Promise<IAccount> {
+      return Promise.resolve({
+        email: 'any_mail@mail.com',
+        id: 'any_id',
+        name: 'any_name',
+        password: 'any_hash'
+      })
+    }
+  }
+  return new UpdateAccountRepStub()
+}
+
+const makeFakeRequest = (): UpdateAccountModel => ({
+  id: 'any_id',
+  email: 'any_mail@mail.com'
+})
+
+describe('DbUpdateAccount', () => {
+  it('Should call repository with correct values', async () => {
+    const { sut, updateAccountRepositoryStub } = makeSut()
+    const updateSpy = jest.spyOn(updateAccountRepositoryStub, 'update')
+    await sut.update(makeFakeRequest())
+    expect(updateSpy).toHaveBeenCalledWith(makeFakeRequest())
+  })
+})
