c0eeba4ec87b9407a41818c5f4cfe077d5a8fce3
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-23 07:24:04 -0300

    refactor: move UpdateUserController export protocols to a single file

--- src/presentation/controller/user/update-user/update-user-controller.ts
+++ src/presentation/controller/user/update-user/update-user-controller.ts
@@ -1,17 +1,24 @@
-import { badRequest, Controller, forbidden, HttpRequest, HttpResponse, IValidation } from "../login/login-controller-protocols";
+import {
+  badRequest,
+  Controller,
+  forbidden,
+  HttpRequest,
+  HttpResponse,
+  IValidation
+} from './update-user-protocols'
 
 export class UpdateUserController implements Controller {
   constructor(private readonly validation: IValidation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error) {
-      return new Promise(resolve => resolve(badRequest(error)))
+      return new Promise((resolve) => resolve(badRequest(error)))
     }
 
     if (httpRequest.user !== httpRequest.body.id) {
-      return new Promise(resolve => resolve(forbidden()))
+      return new Promise((resolve) => resolve(forbidden()))
     }
 
-    return Promise.resolve({statusCode: 200})
+    return Promise.resolve({ statusCode: 200 })
   }
 }
---
+++ src/presentation/controller/user/update-user/update-user-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../protocols/controller'
+export * from '../../../protocols/http'
+export * from '../../../helpers/http-helper'
+export * from '../../../protocols/validation'
