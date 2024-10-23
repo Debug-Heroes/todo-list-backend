092121e605265c91f0c7b0c068cd5f8df6bbc801
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-23 07:09:42 -0300

    feat: ensure UpdateUserController return 400 if validation fails

--- src/presentation/controller/user/update-user/update-user-controller.ts
+++ src/presentation/controller/user/update-user/update-user-controller.ts
@@ -1,9 +1,12 @@
-import { Controller, HttpRequest, HttpResponse, IValidation } from "../login/login-controller-protocols";
+import { badRequest, Controller, HttpRequest, HttpResponse, IValidation } from "../login/login-controller-protocols";
 
 export class UpdateUserController implements Controller {
   constructor(private readonly validation: IValidation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return new Promise(resolve => resolve(badRequest(error)))
+    }
     return Promise.resolve({statusCode: 200})
   }
 }
