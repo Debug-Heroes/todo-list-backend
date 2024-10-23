e3c1c2a5343f9be0dbac237f33e1426ba5ca87c4
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-23 09:45:43 -0300

    feat: ensure PgAccountRepository update call query with correct values

--- src/infra/usecases/db/postgres/repositories/account/pg-account-repository.ts
+++ src/infra/usecases/db/postgres/repositories/account/pg-account-repository.ts
@@ -4,10 +4,12 @@ import { IAccount } from '../../../../../../domain/protocols/account'
 import { IAddAccountModel } from '../../../../../../domain/usecases/users/add-account'
 import { ILoadAccountByEmail } from '../../../../../../domain/usecases/users/load-account'
 import { ILoadAccountById } from '../../../../../../domain/usecases/users/load-account-by-id'
+import { IUpdateAccountRepository } from '../../../../../../data/protocols/db/update-account-repository'
 import { PgHelper } from '../../helpers/pg-helper'
+import { UpdateAccountModel } from '../../../../../../domain/usecases/users/update-account'
 
 export class PgAccountRepository
-  implements IAddAccountRepository, ILoadAccountByEmail, IDeleteAccountRepository, ILoadAccountById
+  implements IAddAccountRepository, ILoadAccountByEmail, IDeleteAccountRepository, ILoadAccountById, IUpdateAccountRepository
 {
   async add(account: IAddAccountModel): Promise<IAccount> {
     const newUser = await PgHelper.query(
@@ -33,5 +35,33 @@ export class PgAccountRepository
   async loadById(id: string): Promise<IAccount | null> {
     const result = await PgHelper.query('SELECT * FROM users WHERE id = $1', [id])
     return Promise.resolve(result.rows?.length > 0 ? result.rows[0] : null)
+  }
+
+  async update(account: UpdateAccountModel): Promise<IAccount> {
+    const queryValues: string[] = []
+    let queryString = 'UPDATE users SET'
+    let queryIndex = 1
+    if (account.email) {
+      queryValues.push(account.email)
+      queryString += ` email = $${queryIndex}`
+      queryIndex++
+    }
+    if (account.name) {
+      queryString += queryIndex > 1 ? ' ' : ''
+      queryValues.push(account.name)
+      queryString += ` name = $${queryIndex}`
+      queryIndex++
+    }
+
+    queryValues.push(account.id)
+    queryString += ` WHERE id = $${queryIndex}`
+
+    await PgHelper.query(queryString, queryValues)
+    return Promise.resolve({
+      id: 'any_id',
+      email: 'any_mail@Mail.cmasdoa',
+      name: 'a',
+      password: 'a'
+    })
   }
 }
