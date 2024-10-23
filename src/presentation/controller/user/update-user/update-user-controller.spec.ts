d2987ba559c936c78ada262cc79ba4ea76c7f6e3
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-23 07:07:02 -0300

    test: ensure UpdateUserController call validation with correct values

---
+++ src/presentation/controller/user/update-user/update-user-controller.spec.ts
@@ -0,0 +1,43 @@
+import { IValidation } from '../../../protocols/validation'
+import { HttpRequest } from '../../../protocols/http'
+import { UpdateUserController } from './update-user-controller'
+
+interface SutTypes {
+  sut: UpdateUserController
+  validationStub: IValidation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub()
+  const sut = new UpdateUserController(validationStub)
+  return {
+    sut,
+    validationStub
+  }
+}
+
+const makeValidationStub = (): IValidation => {
+  class ValidationStub implements IValidation {
+    validate(data: any): Error | null {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    id: 'any_id',
+    name: 'any_name'
+  },
+  user: 'any_id'
+})
+
+describe('UpdateUserController', () => {
+  it('Should call validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    await sut.handle(makeFakeRequest())
+    expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+})
