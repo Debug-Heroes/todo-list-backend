7f88864c22dc03c43ce8f4bf3713aa81de0df7bf
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-24 15:12:49 -0300

    feat: ensure EmailValidation return null if email not received

--- src/presentation/validations/email-validation.ts
+++ src/presentation/validations/email-validation.ts
@@ -5,7 +5,7 @@ import { IEmailValidator } from '../protocols/email-validator'
 export class EmailValidation implements IValidation {
   constructor(private readonly emailValidator: IEmailValidator) {}
   validate(data: any): Error | null {
-    if (!this.emailValidator.isValid(data.email)) {
+    if (data.email && !this.emailValidator.isValid(data.email)) {
       return new InvalidParamError('email')
     }
     return null
