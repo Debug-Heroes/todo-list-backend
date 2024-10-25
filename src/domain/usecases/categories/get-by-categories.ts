bdd72cd3afbfb5662f1107d55b0a5f2b0df8ec48
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-25 11:31:31 -0300

    feat: ensure GetByCategoriesController works correctly

---
+++ src/domain/usecases/categories/get-by-categories.ts
@@ -0,0 +1,9 @@
+import { ICategory } from "../../protocols/category"
+
+export interface IGetByCategories {
+  getBy(values: IGetByCategoriesModel): Promise<ICategory[]>
+}
+
+export interface IGetByCategoriesModel {
+  name: string
+}
---
+++ src/presentation/controller/categories/get/get-by-categories-controller.ts
@@ -0,0 +1,20 @@
+import { IGetByCategories } from "../../../../domain/usecases/categories/get-by-categories";
+import { badRequest } from "../../../helpers/http-helper";
+import { Controller } from "../../../protocols/controller";
+import { HttpRequest, HttpResponse } from "../../../protocols/http";
+import { IValidation } from "../../../protocols/validation";
+
+export class GetByCategoriesController implements Controller {
+  constructor(
+    private readonly validation: IValidation,
+    private  readonly dbGetByCategory: IGetByCategories
+  ) {}
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const error = this.validation.validate(httpRequest.query)
+    if (error) {
+      return new Promise(resolve => resolve(badRequest(error)))
+    }
+    await this.dbGetByCategory.getBy(httpRequest.query)
+    return Promise.resolve({ statusCode: 200 })
+  }
+}
