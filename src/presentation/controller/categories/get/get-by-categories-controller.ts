7b1117fb4c333843ec81b928398f67e0fa9dafaf
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-25 11:34:41 -0300

    feat: ensure GetByCategoriesController return 500 if DbGetByCategories throws

--- src/presentation/controller/categories/get/get-by-categories-controller.ts
+++ src/presentation/controller/categories/get/get-by-categories-controller.ts
@@ -1,5 +1,5 @@
 import { IGetByCategories } from "../../../../domain/usecases/categories/get-by-categories";
-import { badRequest } from "../../../helpers/http-helper";
+import { badRequest, serverError } from "../../../helpers/http-helper";
 import { Controller } from "../../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../../protocols/http";
 import { IValidation } from "../../../protocols/validation";
@@ -10,11 +10,15 @@ export class GetByCategoriesController implements Controller {
     private  readonly dbGetByCategory: IGetByCategories
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.query)
-    if (error) {
-      return new Promise(resolve => resolve(badRequest(error)))
+    try {
+      const error = this.validation.validate(httpRequest.query)
+      if (error) {
+        return new Promise(resolve => resolve(badRequest(error)))
+      }
+      await this.dbGetByCategory.getBy(httpRequest.query)
+      return Promise.resolve({ statusCode: 200 })
+    } catch (error) {
+      return new Promise(resolve => resolve(serverError()))
     }
-    await this.dbGetByCategory.getBy(httpRequest.query)
-    return Promise.resolve({ statusCode: 200 })
   }
 }
