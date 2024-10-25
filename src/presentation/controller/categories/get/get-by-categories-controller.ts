e7319fe5dea7abb3a819281aa26e4f0917c32a78
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-25 11:38:32 -0300

    feat: ensure GetByCategoriesController return categories on DbGetByCategories succeed

--- src/presentation/controller/categories/get/get-by-categories-controller.ts
+++ src/presentation/controller/categories/get/get-by-categories-controller.ts
@@ -1,5 +1,5 @@
 import { IGetByCategories } from "../../../../domain/usecases/categories/get-by-categories";
-import { badRequest, serverError } from "../../../helpers/http-helper";
+import { badRequest, ok, serverError } from "../../../helpers/http-helper";
 import { Controller } from "../../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../../protocols/http";
 import { IValidation } from "../../../protocols/validation";
@@ -15,8 +15,8 @@ export class GetByCategoriesController implements Controller {
       if (error) {
         return new Promise(resolve => resolve(badRequest(error)))
       }
-      await this.dbGetByCategory.getBy(httpRequest.query)
-      return Promise.resolve({ statusCode: 200 })
+      const categories = await this.dbGetByCategory.getBy(httpRequest.query)
+      return Promise.resolve(ok(categories))
     } catch (error) {
       return new Promise(resolve => resolve(serverError()))
     }
