0360c0a573d114016c5ef148173493022b5c78b4
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-28 11:11:34 -0300

    feat: ensure GetAllTasksController return 500 if DbGetAllTasks throws

--- src/presentation/controller/tasks/get/get-all-tasks-controller.ts
+++ src/presentation/controller/tasks/get/get-all-tasks-controller.ts
@@ -1,13 +1,16 @@
 import { IGetAllTasks } from '@domain/usecases/tasks/get-all-tasks'
-import { badRequest } from '../../../helpers/http-helper'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
-import { IValidation } from '../../../protocols/validation'
+import { serverError } from './get-all-tasks-protocols'
 
 export class GetAllTasksController implements Controller {
   constructor(private readonly dbGetAllTasks: IGetAllTasks) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.dbGetAllTasks.getAll(httpRequest.user)
-    return Promise.resolve({ statusCode: 200 })
+    try {
+      await this.dbGetAllTasks.getAll(httpRequest.user)
+      return Promise.resolve({ statusCode: 200 })
+    } catch (error) {
+      return new Promise(resolve => resolve(serverError()))
+    }
   }
 }
