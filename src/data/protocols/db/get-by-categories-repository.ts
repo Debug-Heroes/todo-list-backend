25639f01f472992f44b98f0db5fc606c19d09a2e
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-28 07:00:19 -0300

    feat: ensure DbGetByCategories call repository with correct values

---
+++ src/data/protocols/db/get-by-categories-repository.ts
@@ -0,0 +1,9 @@
+import { ICategory } from "../../../domain/protocols/category";
+
+export interface IGetByCategoriesRepository {
+  getBy(values: GetByCategoriesModel): Promise<ICategory[]>
+}
+
+export interface GetByCategoriesModel {
+  name: string
+}
