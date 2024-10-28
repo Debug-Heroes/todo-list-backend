aa2a8c638455053a92512c29eddf2e5a7c1f004c
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-28 07:00:53 -0300

    test: ensure DbGetByCategories call repository with correct values

---
+++ src/data/usecases/db/categories/get/get-by-categories.spec.ts
@@ -0,0 +1,48 @@
+import { ICategory } from '../../../../../domain/protocols/category'
+import { GetByCategoriesModel, IGetByCategoriesRepository } from '../../../../protocols/db/get-by-categories-repository'
+import { DbGetByCategories } from './get-by-categories'
+
+const makeFakeRequest = (): GetByCategoriesModel => ({
+  name: 'any_name'
+})
+
+interface SutTypes {
+  sut: DbGetByCategories
+  getByCategoriesRepoStub: IGetByCategoriesRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getByCategoriesRepoStub = makeGetByCategoriesRepoStub()
+  const sut = new DbGetByCategories(getByCategoriesRepoStub)
+  return {
+    sut,
+    getByCategoriesRepoStub
+  }
+}
+
+const makeGetByCategoriesRepoStub = (): IGetByCategoriesRepository => {
+  class GetByRepoStub implements IGetByCategoriesRepository {
+    getBy(values: GetByCategoriesModel): Promise<ICategory[]> {
+      return Promise.resolve([
+        {
+          id: 'any_id',
+          name: 'any_name'
+        },
+        {
+          id: 'any_id',
+          name: 'any_name'
+        }
+      ])
+    }
+  }
+  return new GetByRepoStub()
+}
+
+describe('DbGetByCategories', () => {
+  it('Should call repository with correct values', async () => {
+    const { sut, getByCategoriesRepoStub } = makeSut()
+    const getBySpy = jest.spyOn(getByCategoriesRepoStub, 'getBy')
+    await sut.getBy(makeFakeRequest())
+    expect(getBySpy).toHaveBeenCalledWith(makeFakeRequest())
+  })
+})
