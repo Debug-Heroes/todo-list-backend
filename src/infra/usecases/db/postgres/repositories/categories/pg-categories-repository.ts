5e09904c13b15c07172a4f38c954f283289b82ba
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-28 07:33:42 -0300

    feat: ensure GetBy call query with correct values

--- src/infra/usecases/db/postgres/repositories/categories/pg-categories-repository.ts
+++ src/infra/usecases/db/postgres/repositories/categories/pg-categories-repository.ts
@@ -1,10 +1,34 @@
-import { IGetAllCategoriesRepository } from "../../../../../../data/protocols/db/get-all-categories-repository";
-import { ICategory } from "../../../../../../domain/protocols/category";
-import { PgHelper } from "../../helpers/pg-helper";
+import { IGetAllCategoriesRepository } from '../../../../../../data/protocols/db/get-all-categories-repository'
+import { ICategory } from '../../../../../../domain/protocols/category'
+import { PgHelper } from '../../helpers/pg-helper'
+import { GetByCategoriesModel, IGetByCategoriesRepository } from '../../../../../../data/protocols/db/get-by-categories-repository'
 
-export class PgCategoriesRepository implements IGetAllCategoriesRepository {
+export class PgCategoriesRepository
+  implements IGetAllCategoriesRepository, IGetByCategoriesRepository
+{
   async getAll(): Promise<ICategory[]> {
     const categories = await PgHelper.query('SELECT * FROM categories')
-    return new Promise(resolve => resolve(categories.rows))
+    return new Promise((resolve) => resolve(categories.rows))
+  }
+
+  async getBy(values: GetByCategoriesModel): Promise<ICategory[]> {
+    const databaseValues = ['name']
+    let queryLength = 1
+    let queryString = 'SELECT * FROM categories WHERE '
+    const queryValues: string[] = []
+    for (const pos of databaseValues as ['name']) {
+      if (values[pos]) {
+        if (queryLength > 1) {
+          queryString += 'AND '
+        }
+        queryString += `${pos} LIKE $${queryLength}`
+        queryValues.push('%' + values[pos] + '%')
+        queryLength++
+      }
+    }
+    console.log(queryString)
+    console.log(queryValues)
+    await PgHelper.query(queryString, queryValues)
+    return Promise.resolve([])
   }
 }
