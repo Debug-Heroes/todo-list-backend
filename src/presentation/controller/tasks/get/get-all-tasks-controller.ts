210808693b690050574f3e52a28f452827c0e27a
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-28 11:35:11 -0300

    feat: ensure GetAllTasksController return tasks on DbGetAllTasks succeed

--- src/presentation/controller/tasks/get/get-all-tasks-controller.ts
+++ src/presentation/controller/tasks/get/get-all-tasks-controller.ts
@@ -1,14 +1,14 @@
 import { IGetAllTasks } from '@domain/usecases/tasks/get-all-tasks'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
-import { serverError } from './get-all-tasks-protocols'
+import { ok, serverError } from './get-all-tasks-protocols'
 
 export class GetAllTasksController implements Controller {
   constructor(private readonly dbGetAllTasks: IGetAllTasks) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.dbGetAllTasks.getAll(httpRequest.user)
-      return Promise.resolve({ statusCode: 200 })
+      const result = await this.dbGetAllTasks.getAll(httpRequest.user)
+      return Promise.resolve(ok(result))
     } catch (error) {
       return new Promise(resolve => resolve(serverError()))
     }
