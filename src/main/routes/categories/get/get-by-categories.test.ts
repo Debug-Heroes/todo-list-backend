02331de0b75a1acbdbdc020ba5bafbe32c3ffb08
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-28 09:10:46 -0300

    test: ensure GetByRoutes return 200 on succeed

---
+++ src/main/routes/categories/get/get-by-categories.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import app from '../../../config/app'
+import { PgHelper } from '../../../../infra/usecases/db/postgres/helpers/pg-helper'
+import { TestPoolConfig } from '../../../../infra/usecases/db/postgres/test/pg-pool-config'
+import { NextFunction } from 'express'
+
+jest.mock(
+  '../../../adapters/middleware-adapter',
+  () => () => async (req: Request, res: Response, next: NextFunction) => next()
+)
+
+describe('GetByRoutes', () => {
+  beforeAll(async () => {
+    PgHelper.connect(TestPoolConfig).then(() => {})
+  })
+  afterAll(async () => {
+    PgHelper.disconnect().then(() => {})
+  })
+  it('Should return 200 on succeed', async () => {
+    await PgHelper.query('INSERT INTO categories VALUES($1, $2)', ['any_id', 'any_name'])
+    await request(app)
+      .get('/api/categories/query?name=any_name')
+      .expect(200)
+  })
+})
