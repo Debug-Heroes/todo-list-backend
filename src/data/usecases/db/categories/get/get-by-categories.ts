ccc9ac7da30d4280bf1f2e6556a65608dc5531ba
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-28 07:12:40 -0300

    feat: ensure DbGetByCategories return categories on repository succeed

--- src/data/usecases/db/categories/get/get-by-categories.ts
+++ src/data/usecases/db/categories/get/get-by-categories.ts
@@ -1,11 +1,14 @@
-import { ICategory } from "../../../../../domain/protocols/category";
-import { IGetByCategories, IGetByCategoriesModel } from "../../../../../domain/usecases/categories/get-by-categories";
-import { IGetByCategoriesRepository } from "../../../../protocols/db/get-by-categories-repository";
+import { ICategory } from '../../../../../domain/protocols/category'
+import {
+  IGetByCategories,
+  IGetByCategoriesModel
+} from '../../../../../domain/usecases/categories/get-by-categories'
+import { IGetByCategoriesRepository } from '../../../../protocols/db/get-by-categories-repository'
 
 export class DbGetByCategories implements IGetByCategories {
   constructor(private readonly repository: IGetByCategoriesRepository) {}
   async getBy(values: IGetByCategoriesModel): Promise<ICategory[]> {
-    await this.repository.getBy(values)
-    return Promise.resolve([])
+    const result = await this.repository.getBy(values)
+    return new Promise(resolve => resolve((result)))
   }
 }
