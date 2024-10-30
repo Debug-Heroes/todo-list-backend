b4b616c1064313e70c683d196ded017665a6231d
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-30 06:18:16 -0300

    test: ensure DbGetAllTasks call repository with correct values

---
+++ src/data/usecases/db/tasks/db-get-all-tasks.spec.ts
@@ -0,0 +1,40 @@
+import { IGetAllTasksRepository } from "@data/protocols/db/get-all-tasks-repository"
+import { ITask } from "@domain/protocols/task"
+import { DbGetAllTasks } from "./db-get-all-tasks"
+
+interface SutTypes {
+  sut: DbGetAllTasks
+  repository: IGetAllTasksRepository
+}
+
+const makeSut = (): SutTypes => {
+  const repository = makeGetAllRepository()
+  const sut = new DbGetAllTasks(repository)
+  return {
+    sut,
+    repository
+  }
+}
+
+const makeGetAllRepository = (): IGetAllTasksRepository => {
+  class GetAllTasks implements IGetAllTasksRepository {
+    async getAll(id: string): Promise<ITask[]> {
+      return Promise.resolve([{
+        id: 'any_id',
+        name: 'any_name',
+        text: 'any_text',
+        userId: 'any_user'
+      }])
+    }
+  }
+  return new GetAllTasks()
+}
+
+describe('DbGetAllTasks', () => {
+  it('Should call repository with correct values', async () => {
+    const { sut, repository } = makeSut()
+    const getAllSpy = jest.spyOn(repository, 'getAll')
+    await sut.getAll('any_id')
+    expect(getAllSpy).toHaveBeenCalledWith('any_id')
+  })
+})
