eea141f7bb1e57c29f3be56c74a453197477bd14
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-30 07:00:59 -0300

    feat: ensure PgTasksRepository return tasks on repository succeed

--- src/infra/usecases/db/postgres/tasks/pg-tasks-repository.ts
+++ src/infra/usecases/db/postgres/tasks/pg-tasks-repository.ts
@@ -4,7 +4,7 @@ import { PgHelper } from "../helpers/pg-helper";
 
 export class PgTasksRepository implements IGetAllTasksRepository {
   async getAll(id: string): Promise<ITask[]> {
-    await PgHelper.query('SELECT * FROM tasks WHERE userid = $1', [id])
-    return Promise.resolve([])
+    const result = await PgHelper.query('SELECT * FROM tasks WHERE userid = $1', [id])
+    return new Promise(resolve => resolve(result.rows))
   }
 }
