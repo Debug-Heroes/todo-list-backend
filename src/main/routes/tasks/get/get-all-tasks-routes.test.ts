3879b5184cb07946b397b910992cc20e930cdb6b
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-30 08:03:35 -0300

    test: ensure get all tasks route return 200 on succeed

---
+++ src/main/routes/tasks/get/get-all-tasks-routes.test.ts
@@ -0,0 +1,36 @@
+import { PgHelper } from '@infra/usecases/db/postgres/helpers/pg-helper'
+import { TestPoolConfig } from '@infra/usecases/db/postgres/test/pg-pool-config'
+import app from '@main/config/app'
+import { NextFunction } from 'express'
+import request from 'supertest'
+
+jest.mock('../../../adapters/middleware-adapter', () => () => async (req: any, res: any, next: NextFunction) => {
+  req.user = 'any_id'
+  next()
+})
+
+describe('GetAllTasksRoutes', () => {
+  beforeAll(async () => {
+    PgHelper.connect(TestPoolConfig)
+    return
+  })
+  beforeEach(async () => {
+    await PgHelper.query('DELETE FROM tasks')
+    await PgHelper.query('DELETE FROM users')
+    return
+  })
+  afterEach(async () => {
+    await PgHelper.query('DELETE FROM tasks')
+    await PgHelper.query('DELETE FROM users')
+    return
+  })
+  afterAll(async () => {
+    PgHelper.disconnect()
+    return
+  })
+  it('Should return 200 on succeed', async () => {
+    await request(app)
+      .get('/tasks')
+      .expect(200)
+  })
+})
