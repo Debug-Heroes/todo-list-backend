7ae45e90723e4a38bf25289dd13f1cdf000a65fd
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-06 10:19:17 -0300

    feat: ensure GetTasksByCategoryController return 400 if validation fails

--- src/presentation/controller/tasks/get-by/get-tasks-by-category-controller.ts
+++ src/presentation/controller/tasks/get-by/get-tasks-by-category-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from "@presentation/helpers/http-helper";
 import { Controller } from "@presentation/protocols/controller";
 import { HttpRequest, HttpResponse } from "@presentation/protocols/http";
 import { IValidation } from "@presentation/protocols/validation";
@@ -5,7 +6,10 @@ import { IValidation } from "@presentation/protocols/validation";
 export class GetTasksBycCategoryController implements Controller {
   constructor(private readonly validation: IValidation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.query)
+    const error = this.validation.validate(httpRequest.query)
+    if (error) {
+      return new Promise(resolve => resolve(badRequest(error)))
+    }
     return Promise.resolve({statusCode: 200})
   }
 }
