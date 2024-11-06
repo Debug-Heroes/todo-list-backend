b90839f1c62fdae7b338c8d9996abb39af1f1b23
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-06 10:54:25 -0300

    feat: ensure GetTasksByCategoryController return 500 if DbGetTasksByCategory throws

--- src/presentation/controller/tasks/get-by/get-tasks-by-category-controller.ts
+++ src/presentation/controller/tasks/get-by/get-tasks-by-category-controller.ts
@@ -1,5 +1,5 @@
 import { IGetTasksByCategory } from "@domain/usecases/tasks/get-tasks-by-category";
-import { badRequest } from "@presentation/helpers/http-helper";
+import { badRequest, serverError } from "@presentation/helpers/http-helper";
 import { Controller } from "@presentation/protocols/controller";
 import { HttpRequest, HttpResponse } from "@presentation/protocols/http";
 import { IValidation } from "@presentation/protocols/validation";
@@ -7,11 +7,15 @@ import { IValidation } from "@presentation/protocols/validation";
 export class GetTasksBycCategoryController implements Controller {
   constructor(private readonly validation: IValidation, private readonly dbGetTasksByCategory: IGetTasksByCategory) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.query)
-    if (error) {
-      return new Promise(resolve => resolve(badRequest(error)))
+    try {
+      const error = this.validation.validate(httpRequest.query)
+      if (error) {
+        return new Promise(resolve => resolve(badRequest(error)))
+      }
+      await this.dbGetTasksByCategory.getByCategory(httpRequest.query)
+      return Promise.resolve({statusCode: 200})
+    } catch (error) {
+      return new Promise(resolve => resolve(serverError()))
     }
-    await this.dbGetTasksByCategory.getByCategory(httpRequest.query)
-    return Promise.resolve({statusCode: 200})
   }
 }
