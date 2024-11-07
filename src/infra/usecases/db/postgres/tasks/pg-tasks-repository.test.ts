fa154ed44d34ae270ece440e63ce7eb3bdd80a21
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-07 16:50:49 -0300

    test: ensure PgTasksRepository GetByCategory call query with correct values

--- src/infra/usecases/db/postgres/tasks/pg-tasks-repository.test.ts
+++ src/infra/usecases/db/postgres/tasks/pg-tasks-repository.test.ts
@@ -1,3 +1,4 @@
+import { GetTasksByCategoryModel } from "@domain/usecases/tasks/get-tasks-by-category"
 import { PgHelper } from "../helpers/pg-helper"
 import { TestPoolConfig } from "../test/pg-pool-config"
 import { PgTasksRepository } from "./pg-tasks-repository"
@@ -8,11 +9,15 @@ describe('PgTasksRepository', () => {
     return
   })
   beforeEach(async () => {
+    await PgHelper.query('DELETE FROM taskByCategory')
+    await PgHelper.query('DELETE FROM categories')
     await PgHelper.query('DELETE FROM tasks')
     await PgHelper.query('DELETE FROM users')
     return
   })
   afterEach(async () => {
+    await PgHelper.query('DELETE FROM taskByCategory')
+    await PgHelper.query('DELETE FROM categories')
     await PgHelper.query('DELETE FROM tasks')
     await PgHelper.query('DELETE FROM users')
     return
@@ -40,6 +45,18 @@ describe('PgTasksRepository', () => {
       expect(result[0].id).toBeTruthy()
       expect(result[0].name).toBe('any_name')
       expect(result[0].text).toBe('any_text')
+    })
+  })
+  describe('GetByCategory', () => {
+    const makeFakeRequest = (): GetTasksByCategoryModel => ({
+      categoryId: 'any_id',
+      id: 'any_id'
+    })
+    it('Should call query with correct values', async () => {
+      const sut = new PgTasksRepository()
+      const querySpy = jest.spyOn(PgHelper, 'query')
+      await sut.getByCategory(makeFakeRequest())
+      expect(querySpy).toHaveBeenCalledWith('SELECT tasks.id, tasks.name, tasks.text, categories.name as categoria  FROM tasks INNER JOIN taskByCategory ON tasks.id = taskByCategory.taskId INNER JOIN categories ON taskByCategory.categoryId = categories.id WHERE taskByCategory.userId = $1 AND categories.id = $2', [makeFakeRequest().id, makeFakeRequest().categoryId])
     })
   })
 })
