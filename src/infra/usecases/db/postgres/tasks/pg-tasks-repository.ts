9cf097b223e684c22c7b6d1a627ee939c44bbea2
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-07 17:41:09 -0300

    feat: ensure PgTasksRepository GetByCategory return tasks with category on succeed

--- src/infra/usecases/db/postgres/tasks/pg-tasks-repository.ts
+++ src/infra/usecases/db/postgres/tasks/pg-tasks-repository.ts
@@ -4,6 +4,7 @@ import { PgHelper } from "../helpers/pg-helper";
 import { IGetTasksByCategoryRepository } from "@data/protocols/db/get-tasks-by-category-repository";
 import { TaskByCategory } from "@domain/protocols/task-by-category";
 import { GetTasksByCategoryModel } from "@domain/usecases/tasks/get-tasks-by-category";
+import { ICategory } from "@domain/protocols/category";
 
 export class PgTasksRepository implements IGetAllTasksRepository, IGetTasksByCategoryRepository {
   async getAll(id: string): Promise<ITask[]> {
@@ -12,7 +13,20 @@ export class PgTasksRepository implements IGetAllTasksRepository, IGetTasksByCat
   }
 
   async getByCategory(model: GetTasksByCategoryModel): Promise<TaskByCategory[]> {
-    const result = await PgHelper.query('SELECT tasks.id, tasks.name, tasks.text, categories.name as categoria  FROM tasks INNER JOIN taskByCategory ON tasks.id = taskByCategory.taskId INNER JOIN categories ON taskByCategory.categoryId = categories.id WHERE taskByCategory.userId = $1 AND categories.id = $2', [model.id, model.categoryId])
-    return new Promise(resolve => resolve(result.rows))
+    const result = await PgHelper.query('SELECT tasks.id, tasks.name, tasks.text, taskByCategory.userId AS userId, categories.name AS categoria  FROM tasks INNER JOIN taskByCategory ON tasks.id = taskByCategory.taskId INNER JOIN categories ON taskByCategory.categoryId = categories.id WHERE taskByCategory.userId = $1 AND categories.id = $2', [model.id, model.categoryId])
+    const foundTasks: TaskByCategory[] = []
+    for (const task of result.rows) {
+      const categorias: ICategory[] = result.rows.filter(fTask => {
+        return fTask.categoryId === task.categoryId
+      })
+      foundTasks.push({
+        id: task.id,
+        name: task.name,
+        text: task.text,
+        userId: result.rows[0].userid,
+        categories: categorias
+      })
+    }
+    return new Promise(resolve => resolve(foundTasks))
   }
 }
