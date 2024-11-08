348eee75126f742404ad1f7a9f40a3cc69e4e07f
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-08 11:51:19 -0300

    feat: created http response helper add

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,13 +1,8 @@
+import { HttpResponse } from '../protocols/http'
 import { AccessDeniedError } from '../errors/access-denied-error'
 import { ServerError } from '../errors/server-error'
 import { Unauthorized } from '../errors/unauthorized-error'
 import { NotFoundError } from '../errors/not-found-error'
-import { HttpResponse } from '../protocols/http'
-
-export const badRequest = (error: Error): HttpResponse => ({
-  statusCode: 400,
-  body: error.message
-})
 
 export const serverError = (): HttpResponse => ({
   statusCode: 500,
@@ -18,17 +13,23 @@ export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
 })
-
-export const forbidden = (): HttpResponse => ({
-  statusCode: 403,
-  body: new AccessDeniedError().message
+export const created = (data: any): HttpResponse => ({
+  statusCode: 201,
+  body: data
 })
 
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error.message
+})
 export const unauthorized = (): HttpResponse => ({
   statusCode: 401,
   body: new Unauthorized().message
 })
-
+export const forbidden = (): HttpResponse => ({
+  statusCode: 403,
+  body: new AccessDeniedError().message
+})
 export const NotFound = (): HttpResponse => ({
   statusCode: 404,
   body: new NotFoundError().message
