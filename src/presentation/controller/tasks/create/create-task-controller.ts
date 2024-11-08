d2ec7dbedd8494c30b54f2fc2039105cc7868162
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-08 11:06:06 -0300

    feat: ensure CreateTaskController call validation with correct values

--- src/presentation/controller/tasks/create/create-task-controller.ts
+++ src/presentation/controller/tasks/create/create-task-controller.ts
@@ -1,11 +1,15 @@
+import { badRequest } from '@presentation/helpers/http-helper'
 import { Controller } from '@presentation/protocols/controller'
-import { HttpRequest, HttpResponse } from '@presentation/protocols/http';
-import { IValidation } from '@presentation/protocols/validation';
+import { HttpRequest, HttpResponse } from '@presentation/protocols/http'
+import { IValidation } from '@presentation/protocols/validation'
 
 export class CreateTaskController implements Controller {
-  constructor (private readonly validation: IValidation) {}
+  constructor(private readonly validation: IValidation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
-    return Promise.resolve({statusCode: 200})
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return new Promise((resolve) => resolve(badRequest(error)))
+    }
+    return Promise.resolve({ statusCode: 200 })
   }
 }
