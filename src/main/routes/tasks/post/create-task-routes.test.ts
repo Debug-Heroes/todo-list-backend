7d81af5ac84ea8ca4f946a868b4b0d78f4f176ba
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-11 16:00:09 -0300

    test: ensure create task route return 200 on succeed

---
+++ src/main/routes/tasks/post/create-task-routes.test.ts
@@ -0,0 +1,41 @@
+import { PgHelper } from '@infra/usecases/db/postgres/helpers/pg-helper'
+import { TestPoolConfig } from '@infra/usecases/db/postgres/test/pg-pool-config'
+import app from '@main/config/app'
+import { NextFunction } from 'express'
+import request from 'supertest'
+
+jest.mock('../../../adapters/middleware-adapter', () => () => async (req: any, res: any, next: NextFunction) => {
+  req.user = 'any_id'
+  next()
+})
+
+describe('CreateTaskRoute', () => {
+  beforeAll(async () => {
+    PgHelper.connect(TestPoolConfig)
+    return
+  })
+  beforeEach(async () => {
+    await PgHelper.query('DELETE FROM tasks')
+    await PgHelper.query('DELETE FROM users')
+    return
+  })
+  afterEach(async () => {
+    await PgHelper.query('DELETE FROM tasks')
+    await PgHelper.query('DELETE FROM users')
+    return
+  })
+  afterAll(async () => {
+    PgHelper.disconnect()
+    return
+  })
+  it('Should return 200 on succeed', async () => {
+    await request(app)
+      .post('/api/tasks')
+      .send({
+        name: 'any_name',
+        text: 'any_text',
+        user_id: 'any_user'
+      })
+      .expect(201)
+  })
+})
