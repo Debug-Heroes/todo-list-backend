55a964be6ca77869f18f094a46397997c0136489
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-11 07:38:34 -0300

    fix: environment variables appointments

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,9 +4,9 @@ env.config()
 export default {
   SERVER_PORT: Number(process.env.SERVER_PORT),
   PG_PORT: Number(process.env.PG_PORT),
-  PG_PASSWORD: process.env.PG_PASSWORD,
-  PG_USER: process.env.PG_USER,
+  PG_PASSWORD: process.env.DATABASE_PASSWORD,
+  PG_USER: process.env.DATABASE_USER,
   PG_HOST: process.env.PG_HOST,
-  PG_DATABASE: process.env.PG_DATABASE,
+  PG_DATABASE: process.env.DATABASE_NAME,
   JWT_SECRET: process.env.JWT_SECRET || 'any_secret'
 }
