941029b9525eb3107c2e0c045788da4802057745
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-11 07:37:40 -0300

    fix: database appointments

--- src/infra/usecases/db/postgres/tasks/pg-tasks-repository.ts
+++ src/infra/usecases/db/postgres/tasks/pg-tasks-repository.ts
@@ -8,17 +8,17 @@ import { ICategory } from "@domain/protocols/category";
 
 export class PgTasksRepository implements IGetAllTasksRepository, IGetTasksByCategoryRepository {
   async getAll(id: string): Promise<ITask[]> {
-    const result = await PgHelper.query('SELECT * FROM tasks WHERE userid = $1', [id])
+    const result = await PgHelper.query('SELECT * FROM tasks WHERE user_id = $1', [id])
     return new Promise(resolve => resolve(result.rows))
   }
 
   async getByCategory(model: GetTasksByCategoryModel): Promise<TaskByCategory[]> {
-    const result = await PgHelper.query('SELECT tasks.id, tasks.name, tasks.text, taskByCategory.userId AS userId, categories.name AS categoria, categories.id AS categoryId  FROM tasks INNER JOIN taskByCategory ON tasks.id = taskByCategory.taskId INNER JOIN categories ON taskByCategory.categoryId = categories.id WHERE taskByCategory.userId = $1 AND categories.id = $2', [model.userId, model.categoryId])
+    const result = await PgHelper.query('SELECT tasks.id, tasks.name, tasks.text, task_by_category.user_id AS user_id, categories.name AS categoria, categories.id AS category_id  FROM tasks INNER JOIN task_by_category ON tasks.id = task_by_category.task_id INNER JOIN categories ON task_by_category.category_id = categories.id WHERE task_by_category.user_id = $1 AND categories.id = $2', [model.userId, model.categoryId])
     const foundTasks: TaskByCategory[] = []
     for (const task of result.rows) {
       const categorias: ICategory[] = []
       for (const pos of result.rows) {
-        if (pos.categoryId === task.categoryId && pos.userid === model.userId && pos.id === task.id) {
+        if (pos.categoryId === task.categoryId && pos.user_id === model.userId && pos.id === task.id) {
           categorias.push({
             id: pos.categoryId,
             name: pos.categoria
@@ -29,7 +29,7 @@ export class PgTasksRepository implements IGetAllTasksRepository, IGetTasksByCat
         id: task.id,
         name: task.name,
         text: task.text,
-        userId: result.rows[0].userid,
+        userId: result.rows[0].user_id,
         categories: categorias
       })
     }
