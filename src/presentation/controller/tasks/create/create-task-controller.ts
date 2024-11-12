1c7b5059973f0a27d8520c9b4a9a050278261163
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-12 06:17:41 -0300

    feat: ensure CreateTaskController call loadUserById with correct values

--- src/presentation/controller/tasks/create/create-task-controller.ts
+++ src/presentation/controller/tasks/create/create-task-controller.ts
@@ -1,4 +1,5 @@
 import { ICreateTask } from '@domain/usecases/tasks/create-task'
+import { ILoadAccountById } from '@domain/usecases/users/load-account-by-id'
 import { badRequest, created } from '@presentation/helpers/http-helper'
 import { Controller } from '@presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '@presentation/protocols/http'
@@ -7,13 +8,15 @@ import { IValidation } from '@presentation/protocols/validation'
 export class CreateTaskController implements Controller {
   constructor(
     private readonly validation: IValidation,
-    private readonly dbCreateTask: ICreateTask
+    private readonly dbCreateTask: ICreateTask,
+    private readonly loadAccountById: ILoadAccountById
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error) {
       return new Promise((resolve) => resolve(badRequest(error)))
     }
+    await this.loadAccountById.loadById(httpRequest.body.userId)
 
     const createdTask = await this.dbCreateTask.create(httpRequest.body)
     return Promise.resolve(created(createdTask))
