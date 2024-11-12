6416a0ce62d96e3f4422454ef5f7a2a7ae6184fc
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-12 06:37:34 -0300

    feat: ensure create tasks route return 201 on succeed

--- src/main/factory/tasks/post/create-tasks-controller.ts
+++ src/main/factory/tasks/post/create-tasks-controller.ts
@@ -2,13 +2,16 @@ import { DbCreateTask } from '@data/usecases/db/tasks/create/db-create-task'
 import { PgTasksRepository } from '@infra/usecases/db/postgres/repositories/tasks/pg-tasks-repository'
 import { CreateTaskController } from '@presentation/controller/tasks/create/create-task-controller'
 import { makeCreateTasksValidation } from './create-tasks-validation'
+import { PgAccountRepository } from '@infra/usecases/db/postgres/repositories/account/pg-account-repository'
 
 export const makeCreateTasksController = (): CreateTaskController => {
   const repository = new PgTasksRepository()
   const dbCreateTask = new DbCreateTask(repository)
+  const loadUserById = new PgAccountRepository()
   const createTaskController = new CreateTaskController(
     makeCreateTasksValidation(),
-    dbCreateTask
+    dbCreateTask,
+    loadUserById
   )
   return createTaskController
 }
