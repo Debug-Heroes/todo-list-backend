fb842234d51a8284d7bdb8203ad5269e3281ff1d
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-18 14:39:10 -0300

    fix: database creation

--- scripts/init-database.sql
+++ scripts/init-database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS db_todo_list;
+CREATE DATABASE db_todo_list;
 \c db_todo_list;
 
 CREATE SCHEMA IF NOT EXISTS sch_todo_list;
@@ -11,16 +11,6 @@ CREATE TABLE IF NOT EXISTS sch_todo_list.users(
 );
 
 
-CREATE TABLE IF NOT EXISTS sch_todo_list.task_by_category(
-  id VARCHAR(100) DEFAULT(gen_random_uuid()) PRIMARY KEY,
-  task_id VARCHAR(100) NOT NULL,
-  category_id VARCHAR(100) NOT NULL,
-  user_id VARCHAR(100) NOT NULL,
-  FOREIGN KEY (task_id) REFERENCES tasks(id),
-  FOREIGN KEY (category_id) REFERENCES categories(id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
 CREATE TABLE IF NOT EXISTS sch_todo_list.tasks(
   id VARCHAR(100) DEFAULT(gen_random_uuid()) PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
@@ -28,7 +18,7 @@ CREATE TABLE IF NOT EXISTS sch_todo_list.tasks(
   user_id VARCHAR(100),
   created_at timestamp DEFAULT(NOW()),
   progress VARCHAR(16) NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES sch_todo_list.users(id)
 );
 
 CREATE INDEX IF NOT EXISTS idx_user_id ON sch_todo_list.tasks(user_id);
@@ -36,4 +26,14 @@ CREATE INDEX IF NOT EXISTS idx_user_id ON sch_todo_list.tasks(user_id);
 CREATE TABLE IF NOT EXISTS sch_todo_list.categories(
   id VARCHAR(100) DEFAULT(gen_random_uuid()) PRIMARY KEY,
   name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sch_todo_list.task_by_category(
+  id VARCHAR(100) DEFAULT(gen_random_uuid()) PRIMARY KEY,
+  task_id VARCHAR(100) NOT NULL,
+  category_id VARCHAR(100) NOT NULL,
+  user_id VARCHAR(100) NOT NULL,
+  FOREIGN KEY (task_id) REFERENCES sch_todo_list.tasks(id),
+  FOREIGN KEY (category_id) REFERENCES sch_todo_list.categories(id),
+  FOREIGN KEY (user_id) REFERENCES sch_todo_list.users(id)
 );
