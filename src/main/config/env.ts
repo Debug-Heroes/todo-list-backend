f13eaa9d705d02e2cc2dc43ac63017e01cac9247
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-18 14:35:40 -0300

    refactor: change DATABASE_PASSWORD environment variable name to PG_PASSWORD

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,7 +4,7 @@ env.config()
 export default {
   SERVER_PORT: Number(process.env.SERVER_PORT),
   PG_PORT: Number(process.env.PG_PORT),
-  PG_PASSWORD: process.env.DATABASE_PASSWORD,
+  PG_PASSWORD: process.env.PG_PASSWORD,
   PG_USER: process.env.DATABASE_USER,
   PG_HOST: process.env.PG_HOST,
   PG_DATABASE: process.env.DATABASE_NAME,
