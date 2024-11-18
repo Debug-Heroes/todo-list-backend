c1a1c985c78e37b6f999bda9cf86580bc6014be6
Daniel <danielfalcaoviktorio@gmail.com>
2024-11-18 17:07:33 -0300

    fix: environment variables with DATABASE in name conflicting with reserved word from postgres

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -5,8 +5,8 @@ export default {
   SERVER_PORT: Number(process.env.SERVER_PORT),
   PG_PORT: Number(process.env.PG_PORT),
   PG_PASSWORD: process.env.PG_PASSWORD,
-  PG_USER: process.env.DATABASE_USER,
+  PG_USER: process.env.PG_USER,
   PG_HOST: process.env.PG_HOST,
-  PG_DATABASE: process.env.DATABASE_NAME,
+  PG_DATABASE: process.env.PG_DATABASE,
   JWT_SECRET: process.env.JWT_SECRET || 'any_secret'
 }
